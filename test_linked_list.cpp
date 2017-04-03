9d38be7976975cbea3e437273c7fff5b0c4f60ff
smacpher <seansdevelopmentemail@gmail.com>
2017-04-02 17:33:01 -0700

    Added more tests to test_linked_list.cpp

--- test_linked_list.cpp
+++ test_linked_list.cpp
@@ -23,24 +23,36 @@ void testLinkedList() {
     cout << TEST_LABEL << "Testing LinkedList class." << endl;
     list = new IntLinkedList();
 
-    cout << TEST_LABEL << "List before populating:" << endl
-        << *list << endl;
+    cout << TEST_LABEL << "List before populating." << endl;
+    cout << *list << endl;
 
-    cout << TEST_LABEL << "Populating list..." << endl;
-
+    cout << TEST_LABEL << "Populating list with push_front()." << endl;
     for (int i = 0; i < NODES; i++) {
         node_p = new IntLNode(i);
         list->push_front(node_p);
     }
 
-    cout << TEST_LABEL << "List after populating:" << endl
-        << *list << endl;
+    cout << TEST_LABEL << "List after populating." << endl;
+    cout << *list << endl;
 
-    cout << TEST_LABEL << "Popping and displaying from front of list" << endl;
-    for (int i = 0; i < NODES; i++) {
-        node_p = list->front();
-        cout << *node_p << endl;
-        list->pop_front();
-    }
+    cout << TEST_LABEL << "Output of front()." << endl;
+    cout << list->front()->to_string() << endl;
+
+    cout << TEST_LABEL << "Displaying list with to_string()." << endl;
+    cout << *list << endl; // Overloaded operator '<<' calls to_string().
+
+    cout << TEST_LABEL << "Deleting list." << endl;
+    delete list;
+
+    cout << TEST_LABEL << "List after deleting." << endl;
+    cout << *list << endl;
+
+    // cout << TEST_LABEL << "Populating list with push_back()." << endl;
+    // for (int i = 0; i < NODES; i++) {
+    //     node_p = new IntLNode(i);
+
+    // }
+
+
 }
 
